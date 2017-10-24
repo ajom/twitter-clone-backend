cd554e1365b9b6d750f2b9c08069736324532a43
ajom <ajom1795@gmail.com>
2017-10-24 17:14:39 -0400

    first commit, added functionality for searching for tweets by hashtag

---
+++ config.js
@@ -0,0 +1,7 @@
+var appSettings = {
+  consumerKey: '2mYnV8z2zDdHV1wOrwFldDZao',
+  consumerSecret: 'HRIO9vlpWBEPcDguJE57aueGJK7w3rDX2YgkWVogSWVce4lJQ7',
+  bearerToken: ''
+};
+
+module.exports = appSettings;
